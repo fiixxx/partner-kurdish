b51dc1e6c0a7bce4b0708c8584792d52fce53ed8
rawendev <82682424+rawendev@users.noreply.github.com>
2021-07-04 17:56:38 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,60 @@
+const Discord = require("discord.js");
+const client = new Discord.Client({disableEveryone: true})
+
+
+client.on('message', message => { 
+
+if (!message.guild) return; 
+
+if (message.content === 'Dark') { 
+ if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+if (message.member.voiceChannel) { 
+
+message.member.voiceChannel.join() 
+
+.then(connection => { 
+
+
+}) 
+
+.catch(console.log); 
+
+} else { 
+
+message.reply('**Please you go to the voice🐱**'); 
+
+} 
+
+} 
+
+});
+
+
+client.on("message" , message => {
+
+if(message.channel.type === 'dm') {
+
+if (message.content.startsWith("https://discord.gg/")) {
+    
+  message.author.send(`  https://discord.gg/F8ng6crDU4`) ///Lin Server Never ///Dark Heilyam
+    
+  client.channels.get("ID Channel Partner").send(
+`> Dark Heilyam Up | By: <@${message.author.id}> 
+${message.content}`)
+}
+    }
+        })
+
+
+
+
+client.on('ready', () => {
+console.log(`ON ${client.guilds.size} Servers`);
+console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`Send Me`) 
+});
+
+
+
+
+client.login("");///Dark Heilyam ///Token
